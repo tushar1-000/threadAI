a560ed1c9624e70d8c32bdab3d20c50bd571372f
Tushar <tusharyada85@gmail.com>
2025-11-10 21:43:26 +0530

    initial backend setup

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import connectDb from './config/db.js';
+import userRouter from './routes/userRoute.js';
+dotenv.config();
+const app =  express();
+const port = process.env.port || 5000
+
+
+connectDb()
+
+app.use(express.json())
+app.use('/api/user', userRouter)
+
+app.listen(port ,  () => {
+    console.log(`Server is running on port ${port}.....`)
+})
