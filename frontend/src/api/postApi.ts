de65b5aeb60d5e48f0041c1069b7a38e9ed5897a
Tushar Yadav <tusharyada85@gmail.com>
2026-01-01 23:15:54 +0530

    Fixed the object destructuring  for getPosts

--- frontend/src/api/postApi.ts
+++ frontend/src/api/postApi.ts
@@ -5,6 +5,6 @@ export const createPostApi = (data:{content:string} ) => {
   return api.post("/post", data); // your backend endpoint
 };
 
-export const getPostApi = (page =1 , limit = 10) =>{
+export const getPostApi = ({page =1 , limit = 10}) =>{
   return api.get(`/post?page=${page}&limit=${limit}`)
 }
